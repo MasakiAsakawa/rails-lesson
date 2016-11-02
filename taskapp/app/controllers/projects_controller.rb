23f7bdbf9e1e283f73d74272a92ed2d3831c9078
m-asakawa <m-asakawa@froide.co.jp>
2016-11-02 10:34:29 +0900

    #17 データを更新しよう

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -21,6 +21,15 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
   def edit
     @project = Project.find(params[:id])
   end
